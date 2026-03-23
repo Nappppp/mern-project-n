9ece634ee99b60097e13edfc7450bb2981777c8c
Nappppp <84198252+Nappppp@users.noreply.github.com>
2026-03-24 03:14:39 +0800

    Refactor ProductService to use centralized token logic

--- client/src/services/product.service.js
+++ client/src/services/product.service.js
@@ -2,89 +2,72 @@ import axios from "axios";
 const API_URL = "https://mern-project-n.onrender.com/api/products";
 
 class ProductService {
-  post(title, description, price) {
-    //有可能有token有可能沒有
-    let token;
+  // 把取 token 的邏輯抽出來，避免重複
+  // 有可能有token有可能沒有
+  getToken() {
     if (localStorage.getItem("user")) {
-      token = JSON.parse(localStorage.getItem("user")).token;
-    } else {
-      token = "";
+      return JSON.parse(localStorage.getItem("user")).token;
     }
+    return "";
+  }
+
+  getHeaders() {
+    return { Authorization: this.getToken() };
+  }
+
+  post(title, description, price) {
     return axios.post(
       API_URL,
       { title, description, price },
-      {
-        headers: {
-          Authorization: token,
-        },
-      }
+      { headers: this.getHeaders() }
     );
-  }
-
-  // 使用買家id，找到買家註冊的產品
-  getEnrolledProducts(_id) {
-    let token;
-    if (localStorage.getItem("user")) {
-      token = JSON.parse(localStorage.getItem("user")).token;
-    } else {
-      token = "";
-    }
-
-    return axios.get(API_URL + "/buyer/" + _id, {
-      headers: {
-        Authorization: token,
-      },
-    });
   }
 
   // 使用廠商 id，來找到廠商擁有的產品
   get(_id) {
-    let token;
-    if (localStorage.getItem("user")) {
-      token = JSON.parse(localStorage.getItem("user")).token;
-    } else {
-      token = "";
-    }
-
     return axios.get(API_URL + "/manufacturer/" + _id, {
-      headers: {
-        Authorization: token,
-      },
+      headers: this.getHeaders(),
+    });
+  }
+
+  // 使用買家id，找到買家註冊的產品
+  getEnrolledProducts(_id) {
+    return axios.get(API_URL + "/buyer/" + _id, {
+      headers: this.getHeaders(),
     });
   }
 
   getProductByName(name) {
-    let token;
-    if (localStorage.getItem("user")) {
-      token = JSON.parse(localStorage.getItem("user")).token;
-    } else {
-      token = "";
-    }
-
     return axios.get(API_URL + "/findByName/" + name, {
-      headers: {
-        Authorization: token,
-      },
+      headers: this.getHeaders(),
+    });
+  }
+
+  // 新增：用產品 ID 取得單一產品（EditProductComponent 需要）
+  getById(_id) {
+    return axios.get(API_URL + "/" + _id, {
+      headers: this.getHeaders(),
     });
   }
 
   enroll(_id) {
-    let token;
-    if (localStorage.getItem("user")) {
-      token = JSON.parse(localStorage.getItem("user")).token;
-    } else {
-      token = "";
-    }
+    return axios.post(API_URL + "/enroll/" + _id, {}, {
+      headers: this.getHeaders(),
+    });
+  }
 
-    return axios.post(
-      API_URL + "/enroll/" + _id,
-      {},
-      {
-        headers: {
-          Authorization: token,
-        },
-      }
-    );
+  // 新增：更新產品（EditProductComponent 需要）
+  patch(_id, productData) {
+    return axios.patch(API_URL + "/" + _id, productData, {
+      headers: this.getHeaders(),
+    });
+  }
+
+  // 新增：刪除產品（ProductComponent 需要）
+  delete(_id) {
+    return axios.delete(API_URL + "/" + _id, {
+      headers: this.getHeaders(),
+    });
   }
 }
 
